f157988df315ad393b946e755d869b22daad0ede
Jerudiel <78433819+Jerudiel@users.noreply.github.com>
2021-06-07 08:06:03 -0500

    Update routes.js

--- src/network/routes.js
+++ src/network/routes.js
@@ -3,8 +3,8 @@ const product = require('../components/product/network');
 const category = require('../components/category/network')
 
 const routes = function (server) {
-    server.use('/products', product);
-    server.use('/categories', category);
+    server.use('/api/products', product);
+    server.use('/api/categories', category);
 }
 
 module.exports = routes;
